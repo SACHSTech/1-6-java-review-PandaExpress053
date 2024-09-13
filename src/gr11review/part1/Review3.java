f44051348b8782b1fe33848e89278982984d9d53
PandaExpress053 <ethan.fung25@ycdsbk12.ca>
2024-09-13 11:03:50 -0400

    Done Review 3 - 5

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,30 @@
 package gr11review.part1;
+import java.io.*;
+import java.util.*;
+import java.lang.Math.*;
+
+/**
+* A program that prints prints the odd numbers from 20 to 100 
+* and prints the numbers from 29 to 2 in decreasing order.
+* @author: E. Fung
+*
+*/
+public class Review3{
+    public static void main(String[] args) throws IOException{
+
+        // Outputting odd numbers between 20 and 100
+        for (int i = 20; i < 101; i++){
+            if (i % 2 == 1){
+                System.out.println(i);
+            }
+        }
+        
+        System.out.println("");
+
+        // Outputting 29 to 2
+        for (int i = 29; i > 1; i--){
+            System.out.println(i);
+        }
+        
+    }
+}
--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,40 @@
 package gr11review.part1;
+import java.io.*;
+import java.util.*;
+import java.lang.Math.*;
+import java.text.DecimalFormat;
+
+/**
+* A program that asks the user how many items he/she would like to purchase. 
+* The program then gets the prices for the items one by one. At the end of entering 
+* items, print out the total, the tax (13%) and the grand total. Import and use the 
+* java.text.DecimalFormat class to output the information in currency format (0.00)
+* @author: E. Fung
+*
+*/
+public class Review4{
+    public static void main(String[] args) throws IOException{
+
+        // Initialize
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+        DecimalFormat df =  new DecimalFormat("0.00");
+
+        int intNumItems;
+        double dblSubTotal = 0;
+
+        // Inputs 
+        System.out.print("How many items do you want to buy? ");
+        intNumItems = Integer.parseInt(key.readLine());
+
+        for (int i = 0; i < intNumItems; i++){
+            System.out.print("Enter the price for item " + (i+1) + ": ");
+            // Calculates subtotal
+            dblSubTotal += Double.parseDouble(key.readLine());
+        }
+
+        // outputs necessary information based on subtotal
+        System.out.println("Subtotal: $" + df.format(dblSubTotal));
+        System.out.println("Tax: $" + df.format(dblSubTotal * 0.13));
+        System.out.println("Total: $" + df.format(dblSubTotal * 1.13));
+    }
+}
